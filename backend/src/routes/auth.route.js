3850657b9b587f41fd29bb4899d3c5c786597d8a
Kumar-Adarsh-Singh <kumaradarshsingh131@gmail.com>
2026-01-10 00:03:05 +0530

    signup controller added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from 'express'
 
+import { signup, login, logout } from '../controllers/auth.controller.js'
+
 const router = express.Router();
 
-router.get('/signup', (req, res) => {
-  res.send('Signup page');
-})
+router.post('/signup', signup);
+router.get('/login', login);
+router.get('/logout', logout);
 
 export default router;
